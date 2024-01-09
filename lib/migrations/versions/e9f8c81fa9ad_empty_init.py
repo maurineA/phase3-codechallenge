7a3d94e4251a96da6a75c98361f8e47cb15162bc
maurineA <maurinawambuia@gmail.com>
2024-01-09 12:52:25 +0300

    initiated the migration process and created the base

---
+++ lib/migrations/versions/e9f8c81fa9ad_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e9f8c81fa9ad
+Revises: 
+Create Date: 2024-01-09 12:51:34.963280
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e9f8c81fa9ad'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
