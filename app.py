15efd581ed6696b7aad8c99c33b5b379a5ea5af6
maurineA <maurinawambuia@gmail.com>
2024-01-11 15:52:24 +0300

    worked on the pipfile packages

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
+import click
+
+@click.command()
+def hello():
+    click.echo("hello maurine")
+
+if __name__ =='__main__':
+    hello()    
+
