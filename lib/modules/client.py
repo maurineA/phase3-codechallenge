eaa69735c38679e05c863006469280daa3f9d521
maurineA <maurinawambuia@gmail.com>
2024-01-12 15:43:50 +0300

    added the classmethod of adding client to the db

--- lib/modules/client.py
+++ lib/modules/client.py
@@ -12,3 +12,9 @@ class Client(Base):
     cars_id = Column(Integer, ForeignKey('cars.id'))
     
     cars = relationship('Car', back_populates='clients')
+
+    @classmethod
+    def add_client(cls, session, contact, name, cars_id):
+        new_client = cls(contact=contact, name=name, cars_id=cars_id)
+        session.add(new_client)
+        session.commit()
